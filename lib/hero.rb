288dfd29c92457b235428d04c03102faa16b7454
Michael Goodwin <mgoodwin@financeit.io>
2016-01-31 13:04:35 -0500

    Step 06 - Animating Hero - Add logic to change hero's image - Add helper methods to check how hero is moving - Add arrays of images to cycle through as hero moves - Keep track of which direction hero is facing

--- lib/hero.rb
+++ lib/hero.rb
@@ -1,11 +1,25 @@
 class Hero
+
+  MOVE_LEFT_IMAGES = [
+    Gosu::Image.new("media/marioLeftMove1.jpg"),
+    Gosu::Image.new("media/marioLeftMove2.jpg"),
+    Gosu::Image.new("media/marioLeftMove3.jpg")
+  ]
+
+  MOVE_RIGHT_IMAGES = [
+    Gosu::Image.new("media/marioRightMove1.jpg"),
+    Gosu::Image.new("media/marioRightMove2.jpg"),
+    Gosu::Image.new("media/marioRightMove3.jpg")
+  ]
+
   def initialize
-    @image     = Gosu::Image.new("media/marioRightMove1.jpg")
-    @x         = 100
-    @y         = 449
-    @x_vel     = 0
-    @y_vel     = 0
-    @hang_time = 0
+    @image        = Gosu::Image.new("media/marioRightMove1.jpg")
+    @x            = 100
+    @y            = 449
+    @x_vel        = 0
+    @y_vel        = 0
+    @hang_time    = 0
+    @facing_right = true
   end
 
   def draw
@@ -13,7 +27,7 @@ class Hero
   end
 
   def update
-    if @hang_time > 0
+    if floating?
       if @hang_time > 10
         @y_vel = -2
       else
@@ -30,19 +44,62 @@ class Hero
       @x_vel = 0
     end
 
-    @x = @x + @x_vel
-    @y = @y + @y_vel
+    move
 
     @x_vel = 0
     @y_vel = 0
   end
 
+  def move
+    @x = @x + @x_vel
+    @y = @y + @y_vel
+    update_image
+  end
+
+  def update_image
+    if floating?
+      if @facing_right
+        @image = Gosu::Image.new("media/marioRightFloat.jpg")
+      else
+        @image = Gosu::Image.new("media/marioLeftFloat.jpg")
+      end
+    elsif @x_vel != 0
+      if @facing_right
+        @image = move_right_image
+      else
+        @image = move_left_image
+      end
+    else
+      if @facing_right
+        @image = Gosu::Image.new("media/marioRightMove1.jpg")
+      else
+        @image = Gosu::Image.new("media/marioLeftMove1.jpg")
+      end
+    end
+  end
+
+  def move_left_image
+    image_index = Gosu::milliseconds / 100 % MOVE_LEFT_IMAGES.size
+    @image = MOVE_LEFT_IMAGES[image_index]
+  end
+
+  def move_right_image
+    image_index = Gosu::milliseconds / 100 % MOVE_RIGHT_IMAGES.size
+    @image = MOVE_RIGHT_IMAGES[image_index]
+  end
+
+  def floating?
+    @hang_time > 0
+  end
+
   def move_left
     @x_vel = -5
+    @facing_right = false
   end
 
   def move_right
     @x_vel = 5
+    @facing_right = true
   end
 
   def jump
